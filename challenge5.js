a7095cde3519321448aacb0d2d872ca6ecfebe4c
Mike Gudenau <mikegudenau@gmail.com>
2023-02-14 14:28:23 -0800

    Add solution

--- challenge5.js
+++ challenge5.js
@@ -14,19 +14,35 @@ consider the data:
 clarifying questions:
 break it down:
 1) declare a variable named locations and assign it to an array of strings
-2) use the array method (.sort) to sort in alphabetical order=
-3) use a for loop to iterate through the array
-4) push a sentence into the array
+2) use the array method (.sort) to sort in alphabetical order
+3) use a for loop to iterate through the locations array. 
+4) push a string a string into an empty array and include the string interpolaiton with ${locations}. print string 
+5) loop through the updated locationSentence array and print. 
 
-research:
+research: i remembered this type of problem from a coding workshop I did a month prior to starting Turing. I took a look and found a similar solution. 
+
 start coding:
 stuck?
+I was having an issue with the code printing with quotation marks and commas (stuck on end of step 4)
+ 'I cant wait to travel to Berlin !',
+  'I cant wait to travel to Delhi !',
+  'I cant wait to travel to New York !',
+  'I cant wait to travel to Paris !',
+  'I cant wait to travel to Vancouver !'
+
 Refactor:
 */
 
 var locations = ["Paris", "Berlin", "Vancouver", "New York", "Delhi"];
 locations.sort();
+var locationSentence = []
 
 for (var i = 0; i < locations.length; i++) {
-    console.log(locations[i]);
+    locationSentence.push(`I cant wait to travel to ${locations[i]}!`);
 }
+
+// console.log(locationSentence[i]); // this did not have the proper formatting identified in in stuck above.
+
+for (let i = 0; i < locationSentence.length; i++) {
+    console.log(locationSentence[i]);
+  }
